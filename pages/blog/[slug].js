ad105fdf48192a554800d5454f46cc9fe7a8cc5d
VaibhavAgarwal452 <>
2022-10-21 00:22:52 +0530

    Changes in blog js file

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import styles from '../../styles/Blog.module.css';
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>This is {slug} page</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page is {slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+          vero et itaque impedit, fugit minus ducimus aspernatur deserunt
+          molestias iure natus recusandae nobis accusantium sed ut dolor ipsa
+          assumenda quam?
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default slug;
