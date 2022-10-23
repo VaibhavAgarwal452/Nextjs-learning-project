1b0dd78e8a69037a6c31dff51c41f766fcb54748
VaibhavAgarwal452 <>
2022-10-23 10:54:06 +0530

    installed axios and make api request for blog page

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,11 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir('blogdata', (err, data) => {
-    if (err) {
-      res.status(500).json({ error: 'There are no blogs' });
-    }
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir('blogdata');
+  let allBlogs = [];
+  let myFile;
+  for (let index = 0; index < data.length; index++) {
+    myFile = await fs.promises.readFile('blogdata/' + data[index]);
+    allBlogs.push(JSON.parse(myFile));
+  }
+  res.status(200).json(allBlogs);
+
+  // fs.readdir('blogdata', (err, data) => {
+  //   if (err) {
+  //     res.status(500).json({ error: 'There are no blogs' });
+  //   }
+  //   res.status(200).json(data);
+  // });
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,28 +1,32 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Blog.module.css';
+import axios from 'axios';
 const blog = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    let { data } = await axios.get('http://localhost:3000/api/blogs');
+    setPosts(data);
+  };
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1> Hunting coders</h1>
         <p className={styles.description}>A blog for hunting coders</p>
         <div className='blogs'>
-          <Link href='/blog/learn-javascript'>
-            <div className={styles.blogItem}>
-              <h3>How to learn javascript</h3>
-              <p>This is just a dummy text</p>
-            </div>
-          </Link>
-
-          <div className='blogItem'>
-            <h3>How to learn javascript</h3>
-            <p>This is just a dummy text</p>
-          </div>
-          <div className='blogItem'>
-            <h3>How to learn javascript</h3>
-            <p>This is just a dummy text</p>
-          </div>
+          {posts.map((item) => {
+            return (
+              <Link key={item.title} href={`/blog/${item.slug}`}>
+                <div className={styles.blogItem}>
+                  <h3>{item.title}</h3>
+                  <p>{item.content.substr(0, 150)}...</p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </main>
     </div>
