9ba08d3f25f919f36cf1fb2e10fd6c0085a43370
VaibhavAgarwal452 <>
2022-10-23 11:23:45 +0530

    added getblog api in slug

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../../styles/Blog.module.css';
+import axios from 'axios';
 
 const slug = () => {
+  const [blog, setBlog] = useState([]);
   const router = useRouter();
-  const { slug } = router.query;
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const { slug } = router.query;
+    console.log(slug);
+    fetchBlog(slug);
+  }, [router.isReady]);
+
+  const fetchBlog = async (slug) => {
+    const { data } = await axios.get(
+      `http://localhost:3000/api/getBlog?slug=${slug}`
+    );
+    setBlog(data);
+  };
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Title of the page is {slug}</h1>
+        <h1>Title of the page is {blog.title}</h1>
         <hr />
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
-          vero et itaque impedit, fugit minus ducimus aspernatur deserunt
-          molestias iure natus recusandae nobis accusantium sed ut dolor ipsa
-          assumenda quam?
-        </div>
+        <div>{blog.content}</div>
       </main>
     </div>
   );
